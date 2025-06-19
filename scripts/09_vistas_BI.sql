76c8c1cc0a797bdaa3fe3b0d0ec4317d3d5eaa42
Nicolas Fernandez Ruoff <nfernandezruoff@frba.utn.edu.ar>
2025-06-19 20:15:12 -0300

    Update 09_vistas_BI.sql

--- scripts/09_vistas_BI.sql
+++ scripts/09_vistas_BI.sql
@@ -1,3 +1,4 @@
+--- == MATI == -- 
 -- -- == Vista Ganancias == --
 CREATE VIEW FORIF_ISTAS.Ganancias
 AS
@@ -92,3 +93,95 @@ JOIN FORIF_ISTAS.HechoPedido hp ON hv.hecho_venta_sucursal = hp.hecho_pedido_suc
 JOIN FORIF_ISTAS.DimTiempo tv ON hv.hecho_venta_tiempo = tv.tiem_id
 JOIN FORIF_ISTAS.DimTiempo tp ON hp.hecho_pedido_tiempo = tp.tiem_id and tv.tiem_cuatri = tp.tiem_cuatri
 GROUP BY hv.hecho_venta_sucursal, tp.tiem_cuatri
+
+
+
+--- == NICO == -- 
+
+-- == vista 7 == --
+-- Promedio de Compras: importe promedio de compras por mes
+CREATE OR ALTER VIEW FORIF_ISTAS.mv_prom_compras_mes
+(  
+	anio, 
+    mes, 
+    suma_precio,
+    cantidad
+)
+WITH SCHEMABINDING
+AS
+SELECT 
+    YEAR(tiem_fecha) AS anio,
+    MONTH(tiem_fecha) AS mes,
+    SUM(hecho_compra_precio_material) AS suma_precio,
+    COUNT_BIG(*) AS cantidad
+FROM FORIF_ISTAS.HechoCompra
+JOIN FORIF_ISTAS.DimTiempo ON FORIF_ISTAS.HechoCompra.hecho_compra_tiempo = FORIF_ISTAS.DimTiempo.tiem_id
+GROUP BY YEAR(tiem_fecha), MONTH(tiem_fecha)
+GO
+
+CREATE UNIQUE CLUSTERED INDEX IX_mv_prom_compras_mes
+ON FORIF_ISTAS.mv_prom_compras_mes (anio, mes)
+GO
+
+
+
+-- == vista 9 == --
+-- Porcentaje de los cumplimientos de envios en los tiempos programados por mes 
+-- Se calcula teniendo en cuenta los envios cumplidos en fecha sobnre el total de envios para el periodo
+
+CREATE OR ALTER VIEW FORIF_ISTAS.mv_prom_envios_cumplidos
+(
+    anio,
+    mes,
+    suma_envios_totales,
+    suma_envios_en_forma
+)
+WITH SCHEMABINDING
+AS
+SELECT 
+    YEAR(tiem_fecha) AS anio,
+    MONTH(tiem_fecha) AS mes,
+    COUNT_BIG(*) AS suma_envios_totales,
+    SUM(hecho_envio_cantidad_en_forma) AS suma_envios_en_forma
+FROM FORIF_ISTAS.HechoEnvio
+JOIN FORIF_ISTAS.DimTiempo 
+    ON FORIF_ISTAS.HechoEnvio.hecho_envio_tiempo = FORIF_ISTAS.DimTiempo.tiem_id
+GROUP BY YEAR(tiem_fecha), MONTH(tiem_fecha)
+GO
+
+
+CREATE UNIQUE CLUSTERED INDEX IX_mv_prom_envios_cumplidos
+ON FORIF_ISTAS.mv_prom_envios_cumplidos (anio, mes)
+GO
+
+
+-- == PRUEBAS == --
+-- PRUEBAS DE LAS VISTAS
+-- Prueba de la vista
+SELECT anio, mes, suma_precio /cantidad as promedio_de_compras
+FROM FORIF_ISTAS.mv_prom_compras_mes
+
+-- select que la contrasta
+-- Prueba de porcetajes de compras por mes (DA IGUAL) 
+SELECT 
+    YEAR(tiem_fecha) AS anio,
+    MONTH(tiem_fecha) AS mes,
+    SUM(hecho_compra_precio_material) AS suma_precio,
+    COUNT_BIG(*) AS cantidad,
+    AVG(CAST(hecho_compra_precio_material AS FLOAT)) AS promedio_precio_directo
+FROM FORIF_ISTAS.HechoCompra
+JOIN FORIF_ISTAS.DimTiempo ON FORIF_ISTAS.HechoCompra.hecho_compra_tiempo = FORIF_ISTAS.DimTiempo.tiem_id
+GROUP BY YEAR(tiem_fecha), MONTH(tiem_fecha)
+ORDER BY anio, mes
+
+
+-- Utilizo el cast porque sino no me pasa a FLOAT y me da un valor entero 
+SELECT anio, mes, (CAST(suma_envios_en_forma AS FLOAT) /suma_envios_totales) * 100 AS promedio_envios_cumplidos
+FROM FORIF_ISTAS.mv_prom_envios_cumplidos
+
+--- == LEO == --
+
+
+--- == FACU == --
+
+
